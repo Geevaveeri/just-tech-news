96021f82fe917264b99c472f7ae3c418933d5b5e
Geevaveeri <59940979+Geevaveeri@users.noreply.github.com>
2021-05-31 18:40:11 -0600

    added password hashing

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,12 @@
-// import the Sequelize constructor from the library
-const Sequelize = require("sequelize");
-require("dotenv").config();
+const Sequelize = require('sequelize');
 
-// create the connection to our databse, pass in your MySQL information for username and password
+require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PW, {
-    host: "localhost",
-    dialect: "mysql",
-    port: 3306
+// create connection to our db
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  host: 'localhost',
+  dialect: 'mysql',
+  port: 3306
 });
 
 module.exports = sequelize;
